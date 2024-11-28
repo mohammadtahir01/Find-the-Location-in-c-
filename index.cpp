9edbaa9437a0350f75e6cc35a1cf9b3ad0f6b176
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2024-11-28 23:51:40 +0530

    first commit

--- index.cpp
+++ index.cpp
@@ -3,25 +3,15 @@ using namespace std;
 int main(){
     int n;
     
-    cout<<"Enter the Location:";
+    cout<<"enter the number:";
     cin>>n;
-    if(n == 1){
-        cout<<"Choose your destination:";
-        cin>>n;
-        if(n == 2){
-            cout<<"Enter destination:";
-            cin>>n;
-             if(n == 3){
-                cout<<"reached";
-            }else{
-                cout<<"input wrong";
-            }
-        }
+    if(n != 0){
+        cout<<n%10;
+        n=n/10;
+        
+        cout<<n%10;
+        n=n/10;
+        
+        cout<<n;
     }
-    else if(n==3){
-        cout<<"reached";
-       
-    }else{
-        cout<<"wrong";
-    }
-} 
+}
